946803496cb9eb40599769ddcc4679c19f71692f
yasir7341 <yasirabdulrahman75@gmail.com>
2022-08-17 23:13:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const { expect } = require("chai");
+
 function scuberGreetingForFeet(){
-  // Write your code here!
+  scuberGreetingForFee = it('gives customers a free sample if the ride is less than or equal to 400 feet')
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  expect (scuberGreetingForFeet(199).to.equal (`This one is on me`))
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  switchOnCharmFromTip = it(`have to pay for a ride > 400`)
 }
